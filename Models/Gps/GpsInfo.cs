60ed417ccde48897ffc7992c9f54c491f0ff3998
Thomas <thomas.letallec@truckonline.pro>
2021-08-17 16:22:03 +0200

    Make GpsInfo attributes nullable

--- Models/Gps/GpsInfo.cs
+++ Models/Gps/GpsInfo.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Truckonline_Demo_Widget.Models
+﻿namespace Truckonline_Demo_Widget.Models
 {
     class GpsInfo
     {
-        public float altitude;
-        public float cap;
-        public float latitude;
-        public float longitude;
-        public float speedKmph;
+        public float? altitude;
+        public float? cap;
+        public float? latitude;
+        public float? longitude;
+        public float? speedKmph;
     }
 }
